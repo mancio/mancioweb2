70cbe7052fe784069bacca47c15fa1f8aeab2926
Andrea Mancini <mancioman2@gmail.com>
2023-07-31 12:00:02 +0200

    set svg random position

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import Background from "./Background";
-
-
+import MoveSVG from './MoveSVG';
+import Cup from './svg/cup.svg';
+import Cloud from './svg/cloud.svg';
+import Car from './svg/car.svg';
 function App() {
+    const svgs = [Car,Cup,Cloud];
+
+    function renderMoveSVGs(svgFiles) {
+        return svgFiles.map((svg, index) => (
+            <MoveSVG key={`svg-${index}`} svgFile={svg} />
+        ));
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-          <Background />
-      </header>
+        {renderMoveSVGs(svgs)}
     </div>
   );
 }
---
+++ src/MoveSVG.js
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import { useSpring, animated, to } from "react-spring";
+
+
+function MoveSVG({ svgFile }) {
+
+    const [position, setPosition] = useState(genRandPos())
+
+    function getFileNameNoExt (file){
+        return file.split("/").pop().split(".")[0];
+    }
+
+    function genRandPos(){
+        return {
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight,
+        };
+    }
+
+    const springProps = useSpring({
+        to: { x: position.x, y: position.y },
+        config: { tension: 200, friction: 20 },
+    });
+
+    return (
+        <div style={{ position: "absolute" }}>
+            <animated.img
+                src={svgFile}
+                alt={getFileNameNoExt(svgFile)}
+                style={{
+                    position: "absolute",
+                    width: "100px",
+                    transform: to(
+                        [springProps.x, springProps.y],
+                        (x, y) => `translate3d(${x}px, ${y}px, 0)`),
+                }}
+            />
+        </div>
+    );
+
+}
+
+export default MoveSVG;
+
