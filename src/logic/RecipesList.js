ef4784622396c184c951a026d6a91a01b195304a
Andrea Mancini <mancioman2@gmail.com>
2023-08-29 15:06:36 +0200

    language selection recipes menu

--- src/logic/RecipesList.js
+++ src/logic/RecipesList.js
@@ -1,57 +1,101 @@
 export const recipesList = [
     {
-        name: 'Panna cotta with strawberries',
+        name: {
+            IT: 'Panna Cotta alle Fragole',
+            EN: 'Strawberry Panna Cotta'
+        },
         picture: "https://www.lacucinaimperfetta.com/wp-content/uploads/2012/04/Panna-cotta-alle-fragole-9.jpg",
         portions: 4,
-        ingredients: [
-            '100 ml milk',
-            '500 ml heavy cream',
-            '100 g sugar',
-            '3 gelatin sheets (6 g)',
-            '1/2 vanilla bean or its contents',
-            'Strawberries'
-        ],
+        ingredients: {
+            IT: [
+                '100 ml latte',
+                '500 ml panna fresca',
+                '100 g zucchero',
+                '3 fogli di gelatina (6 g)',
+                '1/2 baccello di vaniglia o il suo contenuto',
+                'Fragole'
+            ],
+            EN: [
+                '100 ml milk',
+                '500 ml heavy cream',
+                '100 g sugar',
+                '3 gelatin sheets (6 g)',
+                '1/2 vanilla bean or its contents',
+                'Strawberries'
+            ]
+        },
         steps: [
             {
-                description: 'Soak the gelatin sheets in a saucepan with cold water for a few minutes.',
+                description: {
+                    IT: 'Metti a bagno i fogli di gelatina in una pentola con dell\'acqua fredda per alcuni minuti.',
+                    EN: 'Soak the gelatin sheets in a saucepan with cold water for a few minutes.'
+                },
                 picture: '',
             },
             {
-                description: 'In another saucepan, combine the milk, sugar, cream, and vanilla.',
+                description: {
+                    IT: 'In un\'altra pentola, mescola il latte, lo zucchero, la panna e la vaniglia.',
+                    EN: 'In another saucepan, combine the milk, sugar, cream, and vanilla.'
+                },
                 picture: '',
             },
             {
-                description: 'Heat the mixture over medium heat, stirring continuously, until it reaches a temperature close to boiling. You should see some bubbles forming at the edges and steam.',
+                description: {
+                    IT: 'Riscalda la miscela a fuoco medio, mescolando continuamente, fino a quando raggiunge una temperatura vicina all\'ebollizione. Dovresti vedere delle bolle formarsi ai bordi e del vapore.',
+                    EN: 'Heat the mixture over medium heat, stirring continuously, until it reaches a temperature close to boiling. You should see some bubbles forming at the edges and steam.'
+                },
                 picture: '',
             },
             {
-                description: 'Check that the sugar has dissolved completely.',
+                description: {
+                    IT: 'Verifica che lo zucchero si sia completamente sciolto.',
+                    EN: 'Check that the sugar has dissolved completely.'
+                },
                 picture: '',
             },
             {
-                description: 'Remove the pan from the heat.',
+                description: {
+                    IT: 'Togli la pentola dal fuoco.',
+                    EN: 'Remove the pan from the heat.'
+                },
                 picture: '',
             },
             {
-                description: 'Take the gelatin sheets out of the water and squeeze out any excess water.',
+                description: {
+                    IT: 'Togli i fogli di gelatina dall\'acqua e strizzali per eliminare l\'acqua in eccesso.',
+                    EN: 'Take the gelatin sheets out of the water and squeeze out any excess water.'
+                },
                 picture: '',
             },
             {
-                description: 'Add the gelatin to the pan and stir until it has completely dissolved.',
+                description: {
+                    IT: 'Aggiungi la gelatina alla pentola e mescola finché non si sarà completamente sciolta.',
+                    EN: 'Add the gelatin to the pan and stir until it has completely dissolved.'
+                },
                 picture: '',
             },
             {
-                description: 'Pour the mixture into the glasses or containers you will be serving the panna cotta in.',
+                description: {
+                    IT: 'Versa la miscela nei bicchieri o nei contenitori in cui servirai la panna cotta.',
+                    EN: 'Pour the mixture into the glasses or containers you will be serving the panna cotta in.'
+                },
                 picture: '',
             },
             {
-                description: 'Allow the panna cotta to cool to room temperature, then cover and refrigerate until set.',
+                description: {
+                    IT: 'Lascia raffreddare la panna cotta a temperatura ambiente, poi copri e metti in frigorifero fino a quando sarà solidificata.',
+                    EN: 'Allow the panna cotta to cool to room temperature, then cover and refrigerate until set.'
+                },
                 picture: '',
             },
             {
-                description: 'Once the panna cotta has set, cut some strawberries into slices and place them on top of each serving.',
+                description: {
+                    IT: 'Una volta che la panna cotta si sarà solidificata, taglia delle fragole a fette e posizionalele sulla parte superiore di ogni porzione.',
+                    EN: 'Once the panna cotta has set, cut some strawberries into slices and place them on top of each serving.'
+                },
                 picture: '',
             }
         ]
-    }
+    },
+    // ... other recipes ...
 ];
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -3,19 +3,49 @@ import { useNavigate } from 'react-router-dom';
 import {MENU} from "../logic/Names";
 import {removeSpaceLowerCaseString} from "../logic/Functions";
 import {recipesList} from "../logic/RecipesList";
+import {useState} from "react";
 
 function Recipes(){
 
     const navigate = useNavigate();
 
+    const [selectedLanguage, setSelectedLanguage] = useState('EN'); // Default language is English
+
+    const handleLanguageChange = (event) => {
+        const newLanguage = event.target.value;
+        setSelectedLanguage(newLanguage);
+    };
+
+    const getTranslatedRecipeName = (recipe, language) => {
+        if (recipe.name[language]) {
+            return recipe.name[language];
+        } else if (recipe.name.EN) {
+            return recipe.name.EN;
+        } else if (recipe.name.IT) {
+            return recipe.name.IT;
+        } else {
+            return recipe.name;
+        }
+    };
+
     return(
         <div>
             <div className='title'>
                 <p>Recipes</p>
+                <div className='dashboard'>
+                    <p>Language: <select id='languageDropdown' onChange={handleLanguageChange}>
+                        <option value='EN'>English</option>
+                        <option value='IT'>Italian</option>
+                    </select>
+                    </p>
+                </div>
             </div>
-            {recipesList.map((recipe, index) => (
-                <MyButton text={recipe.name} key={index} onPress={()=>navigate(removeSpaceLowerCaseString(recipe.name))}/>
-            ))}
+            {recipesList.map((recipe, index) => {
+                const translatedRecipeName = getTranslatedRecipeName(recipe, selectedLanguage);
+                return (
+                    <MyButton text={translatedRecipeName} key={index} onPress={()=>navigate(removeSpaceLowerCaseString(translatedRecipeName))}/>
+                );
+            })}
             <MyButton text="Back" onPress={()=>navigate(MENU)}/>
         </div>
     );
