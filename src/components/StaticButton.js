5790159edecedbfce2c2fa3674dcec3958efe41f
Andrea Mancini <mancioman2@gmail.com>
2023-11-22 15:44:02 +0100

    custom button fixed

--- src/components/StaticButton.js
+++ src/components/StaticButton.js
@@ -1,28 +1,34 @@
 import {COOKING} from "../logic/Names";
 import cooking from '../pictures/buttons/cooking-button.svg';
-import React from 'react';
+import {Fragment} from 'react';
 
 
 function StaticButton({style, color, text, click}){
 
     const width = window.innerWidth < 400 ? window.innerWidth /1.2 : 500;
+    const fontSize = window.innerWidth < 400 ? 'large': 'x-large';
 
-    function addBrEvery20Chars(text) {
-        const chunkSize = 20;
-        let chunks = [];
-        for (let i = 0; i < text.length; i += chunkSize) {
-            const chunk = text.slice(i, i + chunkSize);
-            chunks.push(chunk);
+    function returnHtmlText(text, charLimit) {
+        const chunks = [];
+        let start = 0;
+
+        while (start < text.length) {
+            let end = start + charLimit;
+            if (end < text.length && text[end] !== ' ' && text[end - 1] !== ' ') {
+                let nextSpace = text.indexOf(' ', end);
+                end = nextSpace !== -1 ? nextSpace : text.length;
+            }
+
+            chunks.push(text.substring(start, end).trim());
+            start = end + 1;
         }
 
-        const jsxArray = chunks.map((chunk, index) => (
-            <React.Fragment key={index}>
+        return chunks.map((chunk, index) => (
+            <Fragment key={index}>
                 {chunk}
                 {index < chunks.length - 1 && <br />}
-            </React.Fragment>
+            </Fragment>
         ));
-
-        return jsxArray;
     }
 
 
@@ -43,7 +49,7 @@ function StaticButton({style, color, text, click}){
         backgroundColor: 'transparent',
         overflow: 'hidden',
         color: color || 'white',
-        fontSize: 'large',
+        fontSize: fontSize,
         textAlign: 'center', // Optionally center the text
     };
 
@@ -53,7 +59,7 @@ function StaticButton({style, color, text, click}){
                 style={{ ...btnStyle, backgroundImage: `url(${getStyle(style)})` }}
                 className='every-button'
                 onClick={click}>
-                {addBrEvery20Chars(text)}
+                {returnHtmlText(text, 20)}
             </button>
         </div>
     )
