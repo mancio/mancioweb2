39f35332cdbba01dff935b3197be7b2e6816d7aa
Andrea Mancini <mancioman2@gmail.com>
2023-12-20 19:41:34 +0100

    try to play mp3

--- src/logic/Functions.js
+++ src/logic/Functions.js
@@ -284,3 +284,71 @@ export function getWater(hydration, flour){
     return Math.round((hydration / 100) * flour);
 }
 
+/////// Fart page stuff
+
+const farts = {
+    Toot : 'fart1',
+    Ripper : 'fart2',
+    Plop : 'fart3',
+    Squit : 'fart4',
+    Raspberry : 'fart5',
+    Squat : 'fart6',
+    Tuppence : 'fart7',
+    Liftoff : 'fart8',
+    Trumpet : 'fart9',
+    Fizzler : 'fart10',
+    Windy : 'fart11',
+    Eine : 'fart12',
+    Fartception: 'fart13',
+    Fartpoint1: 'fart14'
+};
+
+
+const valuesOfFarts = Object.values(farts);
+
+const namesOfFarts = Object.keys(farts);
+
+export const getNamesOfFarts = function (){
+    return namesOfFarts;
+}
+
+const getFartNameFromIndex = function (index){
+    return namesOfFarts[index];
+}
+
+const getIndexFromFartName = function (name){
+    return namesOfFarts.indexOf(name);
+}
+
+const playFart = function (index){
+    const audio = new Audio('http://fartjs.com/farts/'+ valuesOfFarts[index] + '.mp3');
+    audio.play().then(r => console.log("I'm farting!: " + getFartNameFromIndex(index)))
+        .catch(r => console.log(r));
+}
+
+const getRandomIndex = function (){
+    return Math.floor(Math.random() * valuesOfFarts.length);
+}
+
+export const playRandomFart = function (){
+    playFart(getRandomIndex());
+}
+
+export const playArmageddonFart = async function () {
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    for (let i = 0; i <= 50; i++) {
+        playFart(getRandomIndex());
+        await timer(500);
+    }
+}
+
+export const playSingleFart = function (name){
+    const index = getIndexFromFartName(name);
+    playFart(index);
+}
+
+export const askToStop = function (){
+    window.alert('ehehh you wish.....');
+}
+
+////////////// finish fart page stuff
--- src/pages/Fart.js
+++ src/pages/Fart.js
@@ -1,6 +1,8 @@
 import {MENU} from "../logic/Names";
 import BetterButton from "../components/BetterButton";
 import {useNavigate} from "react-router-dom";
+import '../App.css';
+import {askToStop, getNamesOfFarts, playArmageddonFart, playRandomFart, playSingleFart} from "../logic/Functions";
 
 function Fart(){
 
@@ -9,7 +11,19 @@ function Fart(){
     return(
         <div>
             <div className='recipe-box'>
-                <h3>Fart for fun</h3>
+                <h2>Fart for fun</h2>
+                <h3>Click a button and listen a fart 💩</h3>
+                <div className="button-grid">
+                    <BetterButton click={() => playRandomFart()} text="Random Fart"/>
+                    {getNamesOfFarts().map(name => {
+                        return (
+                            <BetterButton click={() => playSingleFart(name)} text={name} key={name}/>
+                        )
+                    })}
+                    <BetterButton click={() => playArmageddonFart()} text="Armageddon"/>
+                    <BetterButton click={() => askToStop()} text="Stop farting"/>
+                </div>
+
             </div>
             <BetterButton text="Back" click={() => navigate(MENU)}/>
         </div>
