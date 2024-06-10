51f6174de0c52fb836b28aa42f2e56c2e5e1a15a
Andrea Mancini <mancioman2@gmail.com>
2024-06-10 17:32:37 +0200

    code refactor less page reload

--- src/pages/RecipesGen.js
+++ src/pages/RecipesGen.js
@@ -1,42 +1,40 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState, useCallback } from "react";
 import {
     changeIngredientQuantity, getLanguagesByCode,
     getRecipeByUrl,
     getRecipeData, getRecipeNameByCodeLang, numberToEmoji,
 } from "../logic/Functions";
-import {EMPTY, ITALIAN, MAGIC_SEPARATOR, RECIPES} from "../logic/Names";
+import { EMPTY, ITALIAN, MAGIC_SEPARATOR, RECIPES } from "../logic/Names";
 import IngredientMultiplier from "../components/IngredientMultiplier";
 import YouTubeLogo from "../pictures/icons/YouTube.svg";
 import BetterButton from "../components/BetterButton";
-import {recipeDataModel, recipeModel} from "../components/recipes/RecipesList";
+import { recipeDataModel, recipeModel } from "../components/recipes/RecipesList";
 
-function RecipesGen(){
-
+function RecipesGen() {
     const navigate = useNavigate();
+    const { recipeName: initialRecipeName } = useParams();
 
-    const [recipeName, setRecipeName] = useState(useParams().recipeName);
+    const [recipeName, setRecipeName] = useState(initialRecipeName);
     const [recipe, setRecipe] = useState(recipeModel);
     const [recipeData, setRecipeData] = useState(recipeDataModel);
     const [multiplier, setMultiplier] = useState(1);
     const [languageList, setLanguageList] = useState([ITALIAN]);
 
-    useEffect(() => {
+    const fetchRecipeData = useCallback((recipeName) => {
         const currentRecipe = getRecipeByUrl(recipeName);
         setRecipe(currentRecipe);
-    }, [recipeName]);
-
-    useEffect(() => {
-        const currentRecipeData = getRecipeData(recipe.text);
+        const currentRecipeData = getRecipeData(currentRecipe.text);
         setRecipeData(currentRecipeData);
-    }, [recipe]);
+        const currentLangList = getLanguagesByCode(currentRecipe.code);
+        setLanguageList(currentLangList);
+    }, []);
 
     useEffect(() => {
-        const currentLangList = getLanguagesByCode(recipe.code);
-        setLanguageList(currentLangList);
-    }, [recipe]);
+        fetchRecipeData(recipeName);
+    }, [fetchRecipeData, recipeName]);
 
-    function findFirstPictureUrl(num){
+    function findFirstPictureUrl(num) {
         // Iterate through the pictures array to find the picture with number 0
         for (let picture of recipeData.pictures) {
             if (picture.number === num) {
@@ -47,33 +45,33 @@ function RecipesGen(){
         return null;
     }
 
-    function changeLanguage(ln){
-        const newRecipe = getRecipeNameByCodeLang(recipe.code, ln);
-        setRecipeName(newRecipe);
+    function changeLanguage(ln) {
+        const newRecipeName = getRecipeNameByCodeLang(recipe.code, ln);
+        setRecipeName(newRecipeName);
     }
 
-    return(
+    return (
         <div className='recipe-box'>
             <div className='title2'>
                 <h3>{recipe.name}</h3>
             </div>
-            <img className='recipe-img' src={findFirstPictureUrl(0)} alt={recipe.name}/>
+            <img className='recipe-img' src={findFirstPictureUrl(0)} alt={recipe.name} />
             <p>{recipeData.servings}</p>
             {MAGIC_SEPARATOR}
-            <IngredientMultiplier multiplier={multiplier} setMultiplier={setMultiplier}/>
+            <IngredientMultiplier multiplier={multiplier} setMultiplier={setMultiplier} />
             <h3>Ingredients</h3>
-            {recipeData.ingredients.map((ingredients, index) => (
+            {recipeData.ingredients.map((ingredient, index) => (
                 <div key={index}>
-                    <p>🔆 {changeIngredientQuantity(ingredients, multiplier)} 🔆</p>
+                    <p>🔆 {changeIngredientQuantity(ingredient, multiplier)} 🔆</p>
                 </div>
             ))}
             {MAGIC_SEPARATOR}
             <h3>Steps:</h3>
             {recipeData.steps.map((step, index) => (
                 <div key={index}>
-                    <p style={{textAlign: 'left'}}>{numberToEmoji(index + 1)} ➼ {step}</p>
-                    {(findFirstPictureUrl(index + 1)) &&
-                        <img className='recipe-img' src={findFirstPictureUrl(index + 1)} alt={`Step ${index + 1}`}/>}
+                    <p style={{ textAlign: 'left' }}>{numberToEmoji(index + 1)} ➼ {step}</p>
+                    {findFirstPictureUrl(index + 1) &&
+                        <img className='recipe-img' src={findFirstPictureUrl(index + 1)} alt={`Step ${index + 1}`} />}
                 </div>
             ))}
             {!recipeData.notes.includes(EMPTY) && (
@@ -88,17 +86,17 @@ function RecipesGen(){
                     {MAGIC_SEPARATOR}
                     <h1>Link to video</h1>
                     <a href={recipeData.video} target="_blank" rel="noopener noreferrer">
-                        <img src={YouTubeLogo} alt="Watch on YouTube" width="100"/>
+                        <img src={YouTubeLogo} alt="Watch on YouTube" width="100" />
                     </a>
                 </div>
             )}
             <div className='language-line'>
                 <h3>Languages:</h3>
                 {languageList.map((language) => (
-                    <BetterButton key={language} text={language} click={() => changeLanguage(language)}/>
+                    <BetterButton key={language} text={language} click={() => changeLanguage(language)} />
                 ))}
             </div>
-            <BetterButton text="Back" click={() => navigate(RECIPES)}/>
+            <BetterButton text="Back" click={() => navigate(RECIPES)} />
         </div>
     );
 }
